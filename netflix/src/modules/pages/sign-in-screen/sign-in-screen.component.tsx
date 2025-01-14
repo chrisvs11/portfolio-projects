d537ba3b3106052365c06245f795bc53aa5a7b3c
Christian Vargas <chrisvs11@hotmail.com>
2025-01-14 09:11:29 -0600

    feat(netflix): add netflix mocked user

--- netflix/src/modules/pages/sign-in-screen/sign-in-screen.component.tsx
+++ netflix/src/modules/pages/sign-in-screen/sign-in-screen.component.tsx
@@ -58,6 +58,7 @@ function onSubmit() {
             <h4>
               <span className={classes.signUp_gray}>New to netflix? </span>
               <a onClick={()=>setSignUp(true)}>Sign Up now</a>
+              <div>Try this: <p>username: mockedUser@mocked.com,</p> <p>password: 1234</p></div>
             </h4>
           </form>
           )}
